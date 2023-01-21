81a9873a57e762174b5d61159736482c118aac61
SauravKalal <saurav.c.kalal@gmail.com>
2023-01-21 09:40:35 +0530

    Programs added

---
+++ Probem_3/testcase2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var input map[string]interface{} = make(map[string]interface{})
+
+	err := json.Unmarshal([]byte(`{"a":{"b":{"c":"d"}}}`), &input)
+	if err != nil {
+		panic(err)
+	}
+
+	var value = GetValue(input, "a")
+
+	fmt.Printf("Value is: %v\n", value)
+}
+
+func GetValue(object map[string]interface{}, key string) interface{} {
+	var value interface{} = object[key]
+	if value != nil {
+		return value
+	}
+
+	for k, v := range object {
+		if reflect.TypeOf(object[k]).Kind() == reflect.Map {
+			value = GetValue(v.(map[string]interface{}), key)
+		}
+
+		if value != nil {
+			return value
+		}
+	}
+
+	return nil
+}
